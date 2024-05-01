9f7e44589b92a9f11496d7d1cc8445e0febf3341
codinglegends101 <codinglegends101@gmail.com>
2024-05-01 18:26:24 +0530

    fix/version

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ def read(fname):
 
 setup(
     name='blendr-cli',
-    version='0.1.2',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
-        'click',      
-        'requests',  
-        'colorama',
-        'GPUtil',
-        'psutil',
-        'keyring',
-        'speedtest-cli',
-        'py-cpuinfo'
+        'click==8.1.7',
+        'requests==2.31.0',
+        'colorama==0.4.6',
+        'GPUtil==1.4.0',
+        'psutil==5.9.8',
+        'keyring==25.2.0',
+        'speedtest-cli==2.1.3',
+        'py-cpuinfo==9.0.0',
+        'python-socketio[client]==5.11.2'
     ],
     entry_points={
         'console_scripts': [
@@ -28,7 +29,7 @@ setup(
     description='Blendr CLI tool for GPU Lending',
     license='MIT',
     keywords='blendr cli',
-    url='https://www.blendr.network',  # Optional project URL
+    url='https://www.blendr.network',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
 )
